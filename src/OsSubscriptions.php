19cb7e51c0dd2b7b5eb948a1ef7d6fdf3984ff89
bitsmyth <nkalakovic@gmail.com>
2025-02-05 15:57:52 +0100

    Update subscription UI and template priority handling
    Disable specific context menu blocks in the subscription item template and ensure template priority supports MolliePayments dependency. Additionally, correct the German label in the configuration for improved clarity.

--- src/OsSubscriptions.php
+++ src/OsSubscriptions.php
@@ -6,4 +6,12 @@ use Shopware\Core\Framework\Plugin;
 
 class OsSubscriptions extends Plugin
 {
+    public function getTemplatePriority(): int
+    {
+        # as we don't want to modify the theme.json with template priority
+        # we will assume that all other plugins have a lower priority than this one,
+        # as modification to customer dashboard are depending on MolliePayments,
+        # which has to be loaded beforehand, so we can inherit from MolliePayments.
+        return 999;
+    }
 }
