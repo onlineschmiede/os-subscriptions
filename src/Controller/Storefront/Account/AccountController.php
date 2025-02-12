de252eadac6036704e9f97fb96253d32c6757ce6
bitsmyth <nkalakovic@gmail.com>
2025-02-12 12:28:15 +0100

    Add cart service and residual purchase logic for subscriptions
    Integrated services and logic to support residual subscription purchases, including handling discounts and associated product line items. Enhanced DI configuration for new dependencies like CartService, LineItemFactoryRegistry, and QuantityPriceCalculator. Implemented additional methods for more dynamic cart manipulation and product retrieval.

--- src/Controller/Storefront/Account/AccountController.php
+++ src/Controller/Storefront/Account/AccountController.php
@@ -7,13 +7,18 @@ use Kiener\MolliePayments\Components\Subscription\SubscriptionManager;
 use Kiener\MolliePayments\Controller\Storefront\AbstractStoreFrontController;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Cart\Cart;
-use Shopware\Core\Checkout\Cart\Order\Transformer\LineItemTransformer;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\LineItemFactoryRegistry;
+use Shopware\Core\Checkout\Cart\Price\QuantityPriceCalculator;
+use Shopware\Core\Checkout\Cart\Price\Struct\QuantityPriceDefinition;
+use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
-use Shopware\Core\Checkout\Order\OrderException;
+use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Aggregation\Metric\SumAggregation;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\EntitySearchResult;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -48,6 +53,26 @@ class AccountController extends AbstractStoreFrontController
      */
     private EntityRepository $orderRepository;
 
+    /**
+    * @var EntityRepository
+    */
+    private EntityRepository $productRepository;
+
+    /**
+    * @var CartService
+    */
+    private CartService $cartService;
+
+    /**
+    * @var LineItemFactoryRegistry
+    */
+    private LineItemFactoryRegistry $lineItemFactoryRegistry;
+
+    /**
+    * @var QuantityPriceCalculator
+    */
+    private QuantityPriceCalculator $quantityPriceCalculator;
+
 
     /**
      * @param SubscriptionPageLoader $pageLoader
@@ -55,22 +80,35 @@ class AccountController extends AbstractStoreFrontController
      * @param LoggerInterface $logger
      * @param SystemConfigService $systemConfigService
      * @param EntityRepository $orderRepository
+     * @param EntityRepository $productRepository
+     * @param CartService $cartService
+     * @param LineItemFactoryRegistry $lineItemFactoryRegistry
+     * @param QuantityPriceCalculator $quantityPriceCalculator
      */
     public function __construct(
         SubscriptionPageLoader $pageLoader,
         SubscriptionManager $subscriptionManager,
         LoggerInterface $logger,
         SystemConfigService $systemConfigService,
-        EntityRepository $orderRepository)
+        EntityRepository $orderRepository,
+        EntityRepository $productRepository,
+        CartService $cartService,
+        LineItemFactoryRegistry $lineItemFactoryRegistry,
+        QuantityPriceCalculator $quantityPriceCalculator,
+    )
     {
         $this->pageLoader = $pageLoader;
         $this->subscriptionManager = $subscriptionManager;
         $this->logger = $logger;
         $this->systemConfigService = $systemConfigService;
         $this->orderRepository = $orderRepository;
+        $this->productRepository = $productRepository;
+        $this->cartService = $cartService;
+        $this->lineItemFactoryRegistry = $lineItemFactoryRegistry;
+        $this->quantityPriceCalculator = $quantityPriceCalculator;
     }
 
-    public function residualPurchase(string $subscriptionId, Request $request, SalesChannelContext $salesChannelContext): Response
+    public function residualPurchase(string $subscriptionId, Request $request, SalesChannelContext $salesChannelContext, Cart $cart): Response
     {
         if (!$this->isLoggedIn($salesChannelContext) ||
             !$this->systemConfigService->get("OsSubscriptions.config.residualPurchaseActive")) {
@@ -79,47 +117,47 @@ class AccountController extends AbstractStoreFrontController
 
         try {
             $criteria = (new Criteria())->addFilter(new EqualsFilter('customFields.mollie_payments.swSubscriptionId', $subscriptionId));
-            $criteria->addAssociation('lineItems');
+            $criteria->addAssociation('lineItems.product');
             $criteria->addAssociation('deliveries');
-            $order = $this->orderRepository->search($criteria, $salesChannelContext->getContext())->first();
+            $criteria->addAggregation(new SumAggregation('sum-amountNet', 'amountNet'));
+            $orders = $this->orderRepository->search($criteria, $salesChannelContext->getContext());
 
-            if ($order->getLineItems() === null) {
-                throw OrderException::missingAssociation('lineItems.product');
+            $initialOrder = $orders->first();
+            $orderCount = count($orders);
+            $maxOrderCount = $this->systemConfigService->get("OsSubscriptions.config.residualPurchaseValidUntilInterval", $salesChannelContext->getSalesChannel()->getId());
+
+            if($orderCount > $maxOrderCount) {
+                # TODO: handle error snippet key
+                return $this->routeToErrorPage(
+                    'molliePayments.subscriptions.account.errorUpdateAddress',
+                    'Error while trying to purchase residually for subscription ' . $subscriptionId . ': orderCount is greater then maxOrderCount'
+                );
             }
 
-            if ($order->getDeliveries() === null) {
-                throw OrderException::missingAssociation('deliveries');
+            foreach($initialOrder->getLineItems() as $orderLineItem) {
+                $hasRentalOption = array_reduce(
+                    $orderLineItem->getPayload()['options'],
+                    fn($carry, $option) => $carry && $option['option'] === 'Mieten',
+                    true
+                );
+
+                if(!$hasRentalOption) {
+                    continue;
+                }
+
+                $nonRentableLineItem = $this->getRelatedLineItemByOrderLineEntity($orderLineItem, $salesChannelContext);
+                $this->cartService->add($cart, $nonRentableLineItem, $salesChannelContext);
             }
 
-            $cart = new Cart(Uuid::randomHex());
-            # $cart->setPrice($order->getPrice());
-            $cart->setCustomerComment($order->getCustomerComment());
-            $cart->setAffiliateCode($order->getAffiliateCode());
-            $cart->setCampaignCode($order->getCampaignCode());
-            $cart->setSource($order->getSource());
-            # $cart->addExtension(self::ORIGINAL_ID, new IdStruct($order->getId()));
-            #$cart->addExtension(self::ORIGINAL_ORDER_NUMBER, new IdStruct($orderNumber));
-            /* NEXT-708 support:
-                - transactions
-            */
+            $residualDiscountLineItem = $this->getResidualDiscountLineItem($orders, $salesChannelContext);
+            $this->cartService->add($cart, $residualDiscountLineItem, $salesChannelContext);
 
-            foreach($order->getLineItems() as $lineItem) {
-
-            }
-
-            $lineItems = LineItemTransformer::transformFlatToNested($order->getLineItems());
-
-            $cart->addLineItems($lineItems);
-//            $cart->setDeliveries(
-//                $this->convertDeliveries($order->getDeliveries(), $lineItems)
-//            );
-
+            # $this->cartService->recalculate($cart, $salesChannelContext);
 
             return $this->redirectToRoute('frontend.checkout.confirm.page');
 
         } catch (\Throwable $exception) {
-
-            # TODO: HANDLE AND DISPLAY CORRECTLY
+            # TODO: handle error snippet key
             return $this->routeToErrorPage(
                 'molliePayments.subscriptions.account.errorUpdateAddress',
                 'Error when updating billing address of subscription ' . $subscriptionId . ': ' . $exception->getMessage()
@@ -140,6 +178,57 @@ class AccountController extends AbstractStoreFrontController
         );
 
         // $checkoutUrl = $this->subscriptionManager->updatePaymentMethodStart($subscriptionId, $redirectUrl, $salesChannelContext->getContext());
+    }
+
+    private function getResidualDiscountLineItem(EntitySearchResult $orders, SalesChannelContext $salesChannelContext): LineItem
+    {
+        $discountLineItem = new LineItem('residual-discount', 'rent_residual_discount', null, 1);
+
+        $discountLineItem->setLabel('Restkauf Rabatt auf Abonnement');
+        $discountLineItem->setGood(false);
+        $discountLineItem->setStackable(false);
+        $discountLineItem->setRemovable(false);
+
+        $acknowledgedPaymentPercentage = $this->systemConfigService
+            ->get("OsSubscriptions.config.residualPurchaseAcknowledgedPaymentPercentage",
+                $salesChannelContext->getSalesChannel()->getId());
+
+        $discount = $orders->getAggregations()->get('sum-amountNet')->getSum() * (0.01 * $acknowledgedPaymentPercentage);
+
+        $definition = new QuantityPriceDefinition(
+            $discount * -1,
+            $orders->getEntities()->first()->getPrice()->getTaxRules(),
+            1
+        );
+
+        $discountLineItem->setPriceDefinition($definition);
+        $discountLineItem->setPrice(
+            $this->quantityPriceCalculator->calculate($definition, $salesChannelContext)
+        );
+
+        return $discountLineItem;
+    }
+
+    /**
+     * Returns related "non-rentable" LineItem that can be added to cart
+     * @param OrderLineItemEntity $orderLineItemEntity
+     * @param SalesChannelContext $salesChannelContext
+     * @return LineItem
+     */
+    private function getRelatedLineItemByOrderLineEntity(OrderLineItemEntity $orderLineItemEntity, SalesChannelContext $salesChannelContext): LineItem
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('parentId', $orderLineItemEntity->getProduct()->getParentId()));
+        $criteria->addFilter(new EqualsFilter('options.name', 'Kaufen'));
+        $criteria->addAssociation('options');
+
+        $parentProduct = $this->productRepository->search($criteria, $salesChannelContext->getContext());
+
+        return $this->lineItemFactoryRegistry->create([
+            'type' => LineItem::PRODUCT_LINE_ITEM_TYPE,
+            'referencedId' => $parentProduct->first()->getId(),
+            'quantity' => $orderLineItemEntity->getQuantity(),
+        ], $salesChannelContext);
     }
 
 
