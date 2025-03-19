40163d6fac3427af96de10c4330afe41ca63f748
mijo-os <mijo@onlineschmiede.at>
2025-03-19 10:02:12 +0100

    swap the stock in order subscriber

--- src/Subscriber/OrderSubscriber.php
+++ src/Subscriber/OrderSubscriber.php
@@ -8,6 +8,8 @@ use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Order\OrderEvents;
+use Shopware\Core\Content\Product\Stock\AbstractStockStorage;
+use Shopware\Core\Content\Product\Stock\StockAlteration;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
@@ -23,6 +25,7 @@ class OrderSubscriber implements EventSubscriberInterface
         private readonly EntityRepository $subscriptionRepository,
         private readonly LoggerInterface $logger,
         private readonly EntityRepository $productRepository,
+        private readonly AbstractStockStorage $stockStorage,
     ) {}
 
     public static function getSubscribedEvents(): array
@@ -48,9 +51,15 @@ class OrderSubscriber implements EventSubscriberInterface
 
                 $this->tagOrder($currentOrder, $event->getContext());
                 $this->cancelAndTagSubscriptionOnResidualPurchase($currentOrder, $event->getContext());
+
+                // check if the ordered products are rentable and out of stock, then borrow stock from the borrow product variant
+                $this->borrowStock($currentOrder, $event->getContext());
             }
         } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
+            $this->logger->error('ABORTED OrderSubscriber:', [
+                'error' => $e->getMessage(),
+                'line' => $e->getLine(),
+            ]);
         }
     }
 
@@ -178,80 +187,231 @@ class OrderSubscriber implements EventSubscriberInterface
         );
     }
 
-    private function borrowStock(OrderEntity $order)
+    private function borrowStock(OrderEntity $order, $context)
     {
-        $customFields = $order->getCustomFields();
+        try {
+            $this->logger->info('STARTED Borrowing stock: Borrowing stock was called', [
+                'orderId' => $order->getId(),
+            ]);
 
-        if (isset($customFields['os_subscriptions']['order_type']) && 'initial' === $customFields['os_subscriptions']['order_type']) {
-            $lineItems = $order->getLineItems();
+            $orderTransactions = $order->getTransactions();
 
-            foreach ($lineItems as $lineItem) {
-                $lineItemPayload = $lineItem->getPayload();
-                $lineItemStock = $lineItemPayload['stock'];
+            if (empty($orderTransactions)) {
+                $this->logger->info('ABORTED Borrowing stock: No transactions found in order', [
+                    'orderId' => $order->getId(),
+                ]);
 
-                if ($lineItemStock < 1) {
-                    $criteria = new Criteria();
-                    $criteria->addFilter(new EqualsFilter('id', $lineItem->getProductId()));
-                    $product = $this->productRepository->search($criteria, Context::createDefaultContext())->first();
+                return;
+            }
 
-                    if ($product) {
-                        // Perform actions with the product
+            $orderTransactionState = $order->getTransactions()->first()->getStateMachineState()->getTechnicalName();
 
-                        $customFields = $product->getCustomFields();
+            if ('paid' !== $orderTransactionState) {
+                $this->logger->info('ABORTED Borrowing stock: Order not paid yet', [
+                    'orderId' => $order->getId(),
+                    'orderTransactionState' => $orderTransactionState,
+                ]);
 
-                        if (!$customFields or !isset($customFields['mollie_payments_product_subscription_enabled'])) {
-                            return;
+                return;
+            }
+
+            $orderCustomFields = $order->getCustomFields();
+
+            if (isset($orderCustomFields['os_subscriptions']['stock_borrowed'])) {
+                $this->logger->info('ABORTED Borrowing stock 0: Stock was already borrowed', [
+                    'orderId' => $order->getId(),
+                    'orderCustomFields' => $orderCustomFields,
+                ]);
+
+                return;
+            }
+
+            if (isset($orderCustomFields['os_subscriptions']['order_type']) && 'initial' == $orderCustomFields['os_subscriptions']['order_type']) {
+                $lineItems = $order->getLineItems();
+
+                if (!$lineItems) {
+                    $this->logger->info('ABORTED Borrowing stock 1: No line items found in order', [
+                        'orderId' => $order->getId(),
+                        'orderCustomFields' => $orderCustomFields,
+                    ]);
+
+                    return;
+                }
+
+                foreach ($lineItems as $lineItem) {
+                    $lineItemPayload = $lineItem->getPayload();
+
+                    if (!isset($lineItemPayload['stock'])) {
+                        $this->logger->info('ABORTED Borrowing stock 2: No line items payload found in order', [
+                            'orderId' => $order->getId(),
+                            'orderCustomFields' => $orderCustomFields,
+                        ]);
+
+                        return;
+                    }
+
+                    $lineItemStock = $lineItemPayload['stock'];
+
+                    if ($lineItemStock < 1) {
+                        $this->logger->info('CONTINUED Borrowing stock: Line items stock found in order was below 1', [
+                            'orderId' => $order->getId(),
+                            'orderCustomFields' => $orderCustomFields,
+                        ]);
+
+                        $criteria = new Criteria();
+                        $criteria->addFilter(new EqualsFilter('id', $lineItem->getProductId()));
+                        $product = $this->productRepository->search($criteria, $context)->first();
+
+                        if ($product) {
+                            // Perform actions with the product
+
+                            $productCustomFields = $product->getCustomFields();
+
+                            if (!$productCustomFields or !isset($productCustomFields['mollie_payments_product_subscription_enabled'])) {
+                                $this->logger->info('ABORTED Borrowing stock 3: No product custom fields found', [
+                                    'orderId' => $order->getId(),
+                                    'orderCustomFields' => $orderCustomFields,
+                                ]);
+
+                                return;
+                            }
+                            // Check if the product is a subscription product
+                            $isSubscriptionProduct = true === $productCustomFields['mollie_payments_product_subscription_enabled'] ? true : false;
+
+                            if (!$isSubscriptionProduct) {
+                                $this->logger->info('ABORTED Borrowing stock 4: Product is not subscription type', [
+                                    'orderId' => $order->getId(),
+                                    'orderCustomFields' => $orderCustomFields,
+                                ]);
+
+                                return;
+                            }
+
+                            // Check if the product has a borrow product variant
+                            if (!isset($productCustomFields['mollie_payments_product_parent_buy_variant'])) {
+                                $this->logger->info('ABORTED Borrowing stock 5: Product has no borrowing variant', [
+                                    'orderId' => $order->getId(),
+                                    'orderCustomFields' => $orderCustomFields,
+                                ]);
+
+                                return;
+                            }
+
+                            $borrowProductVariantId = $productCustomFields['mollie_payments_product_parent_buy_variant'];
+
+                            if (!$borrowProductVariantId) {
+                                return;
+                            }
+
+                            // search for the borrow product variant
+                            // $context = Context::createDefaultContext();
+                            $criteria = new Criteria([$borrowProductVariantId]);
+                            $criteria->addAssociation('customFields');
+
+                            $productBorrowVariant = $this->productRepository->search($criteria, $context)->first();
+
+                            // Check if the borrow product variant is available on stock
+                            if (!$productBorrowVariant and $productBorrowVariant->getAvailableStock() < 1) {
+                                $this->logger->info('ABORTED Borrowing stock 6: Product borrowing variant doesnt exist or has not enough stock to borrow', [
+                                    'orderId' => $order->getId(),
+                                    'orderCustomFields' => $orderCustomFields,
+                                ]);
+
+                                return false;
+                            }
+
+                            // now swap the product stock like this; substract sthe stock from productBorrowVariant by the line item quantity and add it to the product
+
+                            // $productBorrowVariant->setAvailableStock($productBorrowVariant->getAvailableStock() - $lineItem->getQuantity());
+                            // $productBorrowVariant->setStock($productBorrowVariant->getStock() - $lineItem->getQuantity());
+                            // $product->setAvailableStock($product->getAvailableStock() + $lineItem->getQuantity());
+                            // $product->setStock($product->getStock() + $lineItem->getQuantity());
+
+                            $this->logger->info('PROCESSING Borrowing stock: Reducing stock from borrow product variant', [
+                                'lineItemId' => $lineItem->getId(),
+                                'productId' => $lineItem->getProductId(),
+                                'quantity' => $lineItem->getQuantity(),
+                                'productBorrowVariant' => $productBorrowVariant->getId(),
+                                'newStock' => $productBorrowVariant->getStock() - $lineItem->getQuantity(),
+                                'oldStock' => $productBorrowVariant->getStock(),
+                            ]);
+
+                            $this->logger->info('PROCCESING Borrowing stock: Adding stock to subscription variant', [
+                                'lineItemId' => $lineItem->getId(),
+                                'productId' => $lineItem->getProductId(),
+                                'quantity' => $lineItem->getQuantity(),
+                                'product' => $product->getId(),
+                                'newStock' => $product->getStock() + $lineItem->getQuantity(),
+                                'oldStock' => $product->getStock(),
+                            ]);
+
+                            // Update the product stock
+                            $this->productRepository->update([
+                                [
+                                    'id' => $productBorrowVariant->getId(),
+                                    'availableStock' => $productBorrowVariant->getAvailableStock(),
+                                    'stock' => $productBorrowVariant->getStock(),
+                                ],
+                                [
+                                    'id' => $product->getId(),
+                                    'availableStock' => $product->getAvailableStock(),
+                                    'stock' => $product->getStock(),
+                                ],
+                            ], $context);
+
+                            // borrow from the borrow product variant and add to the product
+                            // $this->stockStorage->alter(
+                            //     [new StockAlteration($lineItem->getId(), $lineItem->getProductId(), $productBorrowVariant->getStock() - $lineItem->getQuantity(), $productBorrowVariant->getStock())],
+                            //     $context
+                            // );
+
+                            $this->logger->info('PROCESSED Borrowing stock: Borrowed stock from borrow product variant');
+
+                            // Update the product stock
+                            // $this->stockStorage->alter(
+                            //     [new StockAlteration($lineItem->getId(), $lineItem->getProductId(), $product->getStock() + $lineItem->getQuantity(), $product->getStock())],
+                            //     $context
+                            // );
+
+                            $this->logger->info('PROCESSED Borrowing stock: Added stock to subscription product variant');
+
+                            $orderCustomFields['os_subscriptions']['stock_borrowed'] = true;
+                            $orderCustomFields['os_subscriptions']['stock_borrowed_from'] = $productBorrowVariant->getId();
+                            $orderCustomFields['os_subscriptions']['stock_borrowed_to'] = $product->getId();
+
+                            $this->orderRepository->update([
+                                [
+                                    'id' => $order->getId(),
+                                    'customFields' => $orderCustomFields,
+                                ],
+                            ], $context);
+
+                            $this->logger->info('PROCESSED Borrowing stock: Added borrowed stock data to order custom fields');
                         }
-                        // Check if the product is a subscription product
-                        $isSubscriptionProduct = true === $customFields['mollie_payments_product_subscription_enabled'] ? true : false;
+                    } else {
+                        $this->logger->info('ABORTED Borrowing stock 7: Line items stock was above 1', [
+                            'orderId' => $order->getId(),
+                            'orderCustomFields' => $orderCustomFields,
+                        ]);
 
-                        if (!$isSubscriptionProduct) {
-                            return;
-                        }
-
-                        // Check if the product has a borrow product variant
-                        if (!isset($customFields['mollie_payments_product_parent_buy_variant'])) {
-                            return;
-                        }
-
-                        $borrowProductVariantId = $customFields['mollie_payments_product_parent_buy_variant'];
-
-                        if (!$borrowProductVariantId) {
-                            return;
-                        }
-
-                        // search for the borrow product variant
-                        $context = Context::createDefaultContext();
-                        $criteria = new Criteria([$borrowProductVariantId]);
-                        $criteria->addAssociation('customFields');
-
-                        $productBorrowVariant = $this->productRepository->search($criteria, $context)->first();
-
-                        // Check if the borrow product variant is available on stock
-                        if ($productBorrowVariant and $productBorrowVariant->getAvailableStock() > 0) {
-                            // make the product purchaseable
-                            return true;
-                        }
-
-                        // now swap the product stock like this; substract sthe stock from productBorrowVariant by the line item quantity and add it to the product
-
-                        $productBorrowVariant->setAvailableStock($productBorrowVariant->getAvailableStock() - $lineItem->getQuantity());
-                        $product->setAvailableStock($product->getAvailableStock() + $lineItem->getQuantity());
-
-                        // Update the product stock
-                        $this->productRepository->update([
-                            [
-                                'id' => $productBorrowVariant->getId(),
-                                'availableStock' => $productBorrowVariant->getAvailableStock(),
-                            ],
-                            [
-                                'id' => $product->getId(),
-                                'availableStock' => $product->getAvailableStock(),
-                            ],
-                        ], $context);
+                        return;
                     }
                 }
             }
+
+            $this->logger->info('ABORTED Borrowing stock 8: Order is not of subscription initial type', [
+                'orderId' => $order,
+                'orderCustomFields' => $orderCustomFields,
+            ]);
+
+            return false;
+        } catch (\Exception $e) {
+            $this->logger->error('ABORTED Borrowing stock 9:', [
+                'orderId' => $order,
+                'orderCustomFields' => $order->getCustomFields(),
+                'error' => $e->getMessage(),
+                'line' => $e->getLine(),
+            ]);
         }
     }
 }
